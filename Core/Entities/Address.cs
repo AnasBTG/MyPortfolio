405efffede9f788fd4d85d82fffc037f4284e270
Anass BOUTAGHRASSA <anass.boutaghrassa@gmail.com>
2022-09-23 01:03:10 +0200

    first commit

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class Address : EntityBase
+    {
+        public int Number { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
+
+
